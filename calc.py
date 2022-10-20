fixed total asset calculation

calc.py
@@ -5,7 +5,8 @@ ptsbm, extra, giro ,ptsbc = extract()
 
 #print(giro)
 
-def mon(df,month,year):
+#filter a dataframe with all transactions of the desired month
+def mon(df,month,year): 
 
     mask = df['date'].map(lambda x: x.month) == month   
     b = df[mask]
@@ -20,6 +21,20 @@ def mon(df,month,year):
 
     return df
 
+#generate a dataframe with transactions
+def dfgen(a):
+    dfd = pd.date_range(start='1/1/2022', freq='MS', periods=7)
+    dfd = pd.DataFrame(dfd, columns=['date'])
+    print(dfd)
+    df5 = pd.DataFrame()
+    b = 0
+    for i in range(0,7,1):
+        b = b + a
+        df5 = df5.append({'balance':b}, ignore_index=True)
+    df5 = pd.concat([dfd,df5], axis=1)
+    return df5
+
+#merge two dataframes
 def asset(df1, df2, start, end):
 
     #start = value[0]
@@ -42,7 +57,7 @@ def asset(df1, df2, start, end):
 
     print(df3)
     return df3
-
+#mask to selcet desired range/time period
 def mask(df,start, end):
     mask2 = (df['date'].dt.year >= start) & (df['date'].dt.year <= end)
     df = df.loc[mask2]
@@ -62,24 +77,36 @@ def assetb(df1,df2,df3,df4,start,end):
     dft = dft.fillna(method='ffill')
     dft = dft.drop(columns=[ 'text','authority','value','reference'])
     dft = dft.fillna(0)
-    dft = dft.set_axis(['date', 'giro', 'ptsbm', 'ptsbc', 'extra'], axis=1, inplace=False)
+    dft.set_axis(['date', 'giro', 'ptsbm', 'ptsbc', 'extra'], axis=1, inplace=True)
     
-    dft['total'] = dft['giro'] + dft['ptsbm'] + dft['ptsbc'] + dft['extra']
+    #dft['total'] = dft.apply(lambda x: x['giro'] + x['ptsbm'] + x['ptsbc'] + x['extra'], axis=1)
+    #dft.columns = ['date', 'giro', 'ptsbm', 'ptsbc', 'extra','total']
+    print(type(dft))
+    return dft
+
+
+
+def asset2(df1,df2):
+    dft = pd.merge(df1, df2[["date",  "balance"]], on="date", how="left", )
+    dft = dft.fillna(method='ffill')
+    dft = dft.fillna(0)
     print(dft)
- 
+    print(type(dft))
+    return dft
 
-
-
-
-
-
+def total(df):
+    cols = ['giro','ptsbm','ptsbc','extra','balance']
+    df['total'] = df[cols].sum(axis=1)
+    print(df)
 
 df1 = assetb(giro,ptsbm,ptsbc,extra,2021,2022)
-#df2 = asset(ptsbm,ptsbc,2021,2022)
 
 
-#df_mon = mon(giro,4,2022)
-#print(df_mon)
-    
+a = 10000
+boi = dfgen(a)
+print(type(df1))
 
-
+df3 = asset2(df1,boi)
+print(df3)
+df3 = total(df3)
+print(df3)